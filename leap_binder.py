62af4b3b91232125fdf74b7d02ec6888f37d73ce
ranhomri <ran.homri@tensorleap.ai>
2024-04-03 15:36:33 +0300

    leap binder modifications

--- leap_binder.py
+++ leap_binder.py
@@ -1,64 +1,97 @@
-from typing import List, Union
-
+from typing import List, Dict, Union
 import numpy as np
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.utils import to_categorical
+import numpy.typing as npt
+from keras.datasets import cifar10
 
 # Tensorleap imports
 from code_loader import leap_binder
-from code_loader.contract.datasetclasses import PreprocessResponse 
-from code_loader.contract.enums import Metric, DatasetMetadataType
+from code_loader.contract.enums import LeapDataType
 from code_loader.contract.visualizer_classes import LeapHorizontalBar
+from code_loader.contract.datasetclasses import PreprocessResponse
+
+from dataset_selection.cifar10_resnet.utils import metadata_animal, metadata_fly, metadata_label_name, metadata_gt_label
+from dataset_selection.cifar10_resnet.preprocess import preprocess_func
+from dataset_selection.cifar10_resnet.encoders import input_encoder
+from dataset_selection.cifar10_resnet.config import CONFIG
+
 
 # Preprocess Function
-def preprocess_func() -> List[PreprocessResponse]:
-    (train_X, train_Y), (val_X, val_Y) = mnist.load_data()
-
-    train_X = np.expand_dims(train_X, axis=-1)  # Reshape :,28,28 -> :,28,28,1
-    train_X = train_X / 255                       # Normalize to [0,1]
-    train_Y = to_categorical(train_Y)           # Hot Vector
-    
-    val_X = np.expand_dims(val_X, axis=-1)  # Reshape :,28,28 -> :,28,28,1
-    val_X = val_X / 255                     # Normalize to [0,1]
-    val_Y = to_categorical(val_Y)           # Hot Vector
+def preprocess_func_leap() -> List[PreprocessResponse]:
+    train_X, val_X, train_Y, val_Y, test_x, test_y = preprocess_func()
 
     # Generate a PreprocessResponse for each data slice, to later be read by the encoders.
     # The length of each data slice is provided, along with the data dictionary.
-    # In this example we pass `images` and `labels` that later are encoded into the inputs and outputs 
-    train = PreprocessResponse(length=len(train_X), data={'images': train_X, 'labels': train_Y})
-    val = PreprocessResponse(length=len(val_X), data={'images': val_X, 'labels': val_Y})
-    response = [train, val]
+    train = PreprocessResponse(length=train_X.shape[0],
+                               data={'images': train_X, 'labels': train_Y, 'subset_name': 'train'})
+    val = PreprocessResponse(length=val_X.shape[0],
+                             data={'images': val_X, 'labels': val_Y, 'subset_name': 'val'})
+    test = PreprocessResponse(length=test_x.shape[0],
+                              data={'images': test_x, 'labels': test_y, 'subset_name': 'test'})
+    response = [train, val, test]
     return response
 
+
+def unlabeled_data() -> PreprocessResponse:
+    _, (test_X, _) = cifar10.load_data()
+    return PreprocessResponse(length=1000, data={'images': test_X, 'subset_name': 'unlabeled'})
+
+
 # Input encoder fetches the image with the index `idx` from the `images` array set in
-# the PreprocessResponse data. Returns a numpy array containing the sample's image. 
-def input_encoder(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
-    return preprocess.data['images'][idx].astype('float32')
+# the PreprocessResponse data. Returns a numpy array containing the sample's image.
+def input_encoder_leap(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
+    # return preprocess.data['images'][idx].astype('float32')
+    image = preprocess.data['images'][idx].astype('float32')
+    return input_encoder(image)
+
 
 # Ground truth encoder fetches the label with the index `idx` from the `labels` array set in
 # the PreprocessResponse's data. Returns a numpy array containing a hot vector label correlated with the sample.
-def gt_encoder(idx: int, preprocessing: PreprocessResponse) -> np.ndarray:
-    return preprocessing.data['labels'][idx].astype('float32')
-
-# Metadata functions allow to add extra data for a later use in analysis.
-# This metadata adds the int digit of each sample (not a hot vector).
-def metadata_label(idx: int, preprocess: PreprocessResponse) -> int:
-    one_hot_digit = gt_encoder(idx, preprocess)
-    digit = one_hot_digit.argmax()
-    digit_int = int(digit)
-    return digit_int
+def gt_encoder(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
+    return preprocess.data['labels'][idx].astype('float32')
 
 
-def bar_visualizer(data: np.ndarray) -> LeapHorizontalBar:
-    return LeapHorizontalBar(data, LABELS)
+def metadata_sample_index(idx: int, preprocess: PreprocessResponse) -> int:
+    return idx
 
 
-LABELS = ['0','1','2','3','4','5','6','7','8','9']
+def metadata_dict(idx: int, preprocess: PreprocessResponse) -> Dict[str, Union[float, int, str]]:
+    if preprocess.data['subset_name'] == 'unlabeled':
+        gt_label = -1
+        label_name = 'unlabeled'
+        fly = 'unlabeled'
+        animal = 'unlabeled'
+    else:
+        one_hot_digit = gt_encoder(idx, preprocess)
+        digit = one_hot_digit.argmax()  # Returns the indices of the maximum values along an axis
+        digit_int = int(digit)
+        gt_label = metadata_gt_label(digit_int)
+        label_name = metadata_label_name(digit_int)
+        fly = metadata_fly(digit_int)
+        animal = metadata_animal(digit_int)
+
+    res = {
+        "gt_label": gt_label,
+        "gt_label_name": label_name,
+        "fly": fly,
+        "animal": animal
+    }
+    return res
+
+
+def horizontal_bar_visualizer_with_labels_name(data: npt.NDArray[np.float32]) -> LeapHorizontalBar:
+    labels_names = [CONFIG['LABELS_NAMES'][index] for index in range(data.shape[-1])]
+    return LeapHorizontalBar(data, labels_names)
+
+
 # Dataset binding functions to bind the functions above to the `Dataset Instance`.
-leap_binder.set_preprocess(function=preprocess_func)
-leap_binder.set_input(function=input_encoder, name='image')
+leap_binder.set_preprocess(function=preprocess_func_leap)
+# leap_binder.set_unlabeled_data_preprocess(function=unlabeled_data)
+leap_binder.set_input(function=input_encoder_leap, name='image')
 leap_binder.set_ground_truth(function=gt_encoder, name='classes')
-leap_binder.set_metadata(function=metadata_label, metadata_type=DatasetMetadataType.int, name='label')
-leap_binder.add_prediction(name='classes', labels=LABELS)
-leap_binder.set_visualizer(name='horizontal_bar_classes', function=bar_visualizer, visualizer_type=LeapHorizontalBar.type)
+leap_binder.set_metadata(function=metadata_sample_index, name='sample_index')
+leap_binder.set_metadata(function=metadata_dict, name='metadata')
+leap_binder.set_visualizer(horizontal_bar_visualizer_with_labels_name, 'horizontal_bar_lm', LeapDataType.HorizontalBar)
+leap_binder.add_prediction(name='classes', labels=CONFIG['LABELS_NAMES'])
+
+if __name__ == '__main__':
+    leap_binder.check()
